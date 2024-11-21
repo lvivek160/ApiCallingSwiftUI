bb89c31334ba7001524500a22d1f9e59a9a669a5
vivek8421 <vivek.lokhande@neosoftmail.com>
2024-11-21 11:35:51 +0530

    project Added

---
+++ APICallingSwiftUI_Demo/APICallingSwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallingSwiftUI_DemoApp.swift
+//  APICallingSwiftUI_Demo
+//
+//  Created by Neosoft on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct APICallingSwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
